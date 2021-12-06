d199fc4c78a1f5d28f13040c59f3dca4852411a9
guillaume <guillaume.pomies82@gmail.com>
2021-12-06 14:32:29 +0100

    bdd

--- patient.sql
+++ patient.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 30 nov. 2021 à 08:23
+-- Généré le : lun. 06 déc. 2021 à 13:32
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -18,32 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `medecin`
+-- Base de données : `projet_php`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `medecin`
+-- Structure de la table `patient`
 --
 
-DROP TABLE IF EXISTS `medecin`;
-CREATE TABLE IF NOT EXISTS `medecin` (
-  `Id_medecin` int(11) NOT NULL,
-  `Civilité` char(5) DEFAULT NULL,
-  `Nom` varchar(50) DEFAULT NULL,
-  `Prenom` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Id_medecin`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `patient_`
---
-
-DROP TABLE IF EXISTS `patient_`;
-CREATE TABLE IF NOT EXISTS `patient_` (
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE IF NOT EXISTS `patient` (
   `Num_secu` char(50) NOT NULL,
   `Civilité` char(5) DEFAULT NULL,
   `Nom` varchar(50) DEFAULT NULL,
@@ -53,23 +38,6 @@ CREATE TABLE IF NOT EXISTS `patient_` (
   `Lieu_naissance` varchar(50) DEFAULT NULL,
   `Id_medecin` int(11) NOT NULL,
   PRIMARY KEY (`Num_secu`),
-  KEY `Id_medecin` (`Id_medecin`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `rendez_vous`
---
-
-DROP TABLE IF EXISTS `rendez_vous`;
-CREATE TABLE IF NOT EXISTS `rendez_vous` (
-  `Num_secu` char(50) NOT NULL,
-  `Id_medecin` int(11) NOT NULL,
-  `Date_rdv` date NOT NULL,
-  `Heure_rdv` time NOT NULL,
-  `Durée_rdv` time DEFAULT NULL,
-  PRIMARY KEY (`Num_secu`,`Id_medecin`,`Date_rdv`,`Heure_rdv`),
   KEY `Id_medecin` (`Id_medecin`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
