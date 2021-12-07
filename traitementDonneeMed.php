ad9060b0f3b03b7be2bf6cb021642b54c36e46bb
Hugo Gineste <hugo.gineste1@gmail.com>
2021-12-07 08:12:01 +0100

    Modif

---
+++ traitementDonneeMed.php
@@ -0,0 +1,27 @@
+<?php
+$con = mysqli_connect('localhost', 'root', '','cabinet');
+if (! $con)
+	{
+	die("ne pourrait pas se relier : ". mysql_error ());
+	}
+
+
+
+
+
+
+
+    
+$sql=" INSERT INTO medecin (Civilite,Nom,Prenom) VALUES ('$_POST[c_medecin]','$_POST[n_medecin]','$_POST[p_medecin]')";
+mysqli_query($con, $sql);
+header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');
+
+
+
+
+
+
+
+	
+
+?>  
