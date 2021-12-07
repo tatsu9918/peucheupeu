47c90d980fadc0abc9c9d6f1df2e05bf9aedc983
Hugo Gineste <hugo.gineste1@gmail.com>
2021-12-07 08:37:16 +0100

    Medecin ce rajoute bein dans la bd

--- traitementDonneeMed.php
+++ traitementDonneeMed.php
@@ -5,14 +5,8 @@ if (! $con)
 	die("ne pourrait pas se relier : ". mysql_error ());
 	}
 
-
-
-
-
-
-
-    
-$sql=" INSERT INTO medecin (Civilite,Nom,Prenom) VALUES ('$_POST[c_medecin]','$_POST[n_medecin]','$_POST[p_medecin]')";
+	
+$sql="INSERT INTO medecin (Civilite,Nom,Prenom) VALUES ('$_POST[c_medecin]','$_POST[n_medecin]','$_POST[p_medecin]')";
 mysqli_query($con, $sql);
 header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');
 
--- traitementDonneePat.php
+++ traitementDonneePat.php
@@ -11,9 +11,9 @@ if (! $con)
 $resultat = mysqli_query($con,"SELECT COUNT(Num_secu) FROM patient WHERE Num_secu = '$_POST[secu]'");
 $rox=mysqli_fetch_row($resultat);
 if($rox[0] ==0 ){
-    $resultat = mysqli_query($con,"SELECT COUNT(Num_secu) FROM patient WHERE Num_secu = '$_POST[secu]'");
-    $rox=mysqli_fetch_row($resultat);
-    $sql=" INSERT INTO patient (Num_secu,Civilite,Nom,Prenom,Adresse,Date_naissance,Lieu_naissance) VALUES ('$_POST[secu]','$_POST[civPa]','$_POST[nomPa]','$_POST[prenomPa]','$_POST[adressePa]','$_POST[dt_naissance]','$_POST[l_naissance]')";
+    $resultat = mysqli_query($con,"SELECT Id_medecin FROM medecin,patient WHERE medecin.Nom = '$_POST[n_medecin]' and medecin.Prenom= '$_POST[p_medecin]' ");
+    $val=mysqli_fetch_row($resultat);
+    $sql=" INSERT INTO patient (Num_secu,Civilite,Nom,Prenom,Adresse,Date_naissance,Lieu_naissance,Id_medecin) VALUES ('$_POST[secu]','$_POST[civPa]','$_POST[nomPa]','$_POST[prenomPa]','$_POST[adressePa]','$_POST[dt_naissance]','$_POST[l_naissance]','$val[0]')";
     mysqli_query($con, $sql);
     header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');
 }
