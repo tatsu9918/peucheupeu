499d44ae068e39aa7ef674959fb935d0613c021d
guillaume <guillaume.pomies82@gmail.com>
2021-12-07 10:31:13 +0100

    meta charset

--- traitementDonneeMed.php
+++ traitementDonneeMed.php
@@ -1,14 +1,18 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '','cabinet');
-if (! $con)
+$mysqli = new mysqli('localhost', 'root', '','cabinet');
+if (! $mysqli)
 	{
 	die("ne pourrait pas se relier : ". mysql_error ());
 	}
 
 	
 $sql="INSERT INTO medecin (Civilite,Nom,Prenom) VALUES ('$_POST[c_medecin]','$_POST[n_medecin]','$_POST[p_medecin]')";
-mysqli_query($con, $sql);
-header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');
+$test=$mysqli->query($sql);
+if (!$test)
+	{
+	die("ne pourrait pas se relier : ". $mysqli->error	);
+	}
+// header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');	
 
 
 
