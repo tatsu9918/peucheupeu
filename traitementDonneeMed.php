16a356a65099159cbf698062738fda4791be7bac
Hugo Gineste <hugo.gineste1@gmail.com>
2021-12-13 14:08:38 +0100

    Modfi askip

--- traitementDonneeMed.php
+++ traitementDonneeMed.php
@@ -1,3 +1,4 @@
+
 <?php
 $con = mysqli_connect('localhost', 'root', '','cabinet');
 if (! $con)
@@ -5,9 +6,10 @@ if (! $con)
 	die("ne pourrait pas se relier : ". mysql_error ());
 	}
 
-	
+
 $sql="INSERT INTO medecin (Civilite,Nom,Prenom) VALUES ('$_POST[c_medecin]','$_POST[n_medecin]','$_POST[p_medecin]')";
 mysqli_query($con, $sql);
+
 header('Location: http://localhost/m%c3%a9decin/projet-medecin/patient.html');
 
 
--- traitementDonneePat.php
+++ traitementDonneePat.php
@@ -1,21 +1,32 @@
+
 <?php
-$con = mysqli_connect('localhost', 'root', '','cabinet');
-if (! $con)
+
+
+$mysqli = mysqli_connect('localhost', 'root', '','cabinet');
+if (! $mysqli)
 	{
-	die("ne pourrait pas se relier : ". mysql_error ());
+	die("ne pourrait pas se relier : ". mysqli_error ());
 	}
+    
 
 
 
-
-$resultat = mysqli_query($con,"SELECT COUNT(Num_secu) FROM patient WHERE Num_secu = '$_POST[secu]'");
+$resultat = mysqli_query($mysqli,"SELECT COUNT(Num_secu) FROM patient WHERE Num_secu = '$_POST[secu]'");
 $rox=mysqli_fetch_row($resultat);
 if($rox[0] ==0 ){
-    $resultat = mysqli_query($con,"SELECT Id_medecin FROM medecin WHERE medecin.Nom = '$_POST[n_medecin]' and medecin.Prenom= '$_POST[p_medecin]' ");
+    $resultat = mysqli_query($mysqli,"SELECT Id_medecin FROM medecin WHERE medecin.Nom = '$_POST[n_medecin]' and medecin.Prenom= '$_POST[p_medecin]' ");
     $val=mysqli_fetch_row($resultat);
-    $sql=" INSERT INTO patient (Num_secu,Civilite,Nom,Prenom,Adresse,Date_naissance,Lieu_naissance,Id_medecin) VALUES ('$_POST[secu]','$_POST[civPa]','$_POST[nomPa]','$_POST[prenomPa]','$_POST[adressePa]','$_POST[dt_naissance]','$_POST[l_naissance]','$val[0]')";
-    mysqli_query($con, $sql);
     
+    $sql=" INSERT INTO patient (Num_secu,Civilite,Nom,Prenom,Adresse,Date_naissance,Lieu_naissance,Id_medecin) VALUES ('$_POST[secu]','$_POST[civPa]','$_POST[nomPa]','$_POST[prenomPa]','$_POST[adressePa]','$_POST[dt_naissance]','$_POST[l_naissance]','$val[0]'";
+    
+    $test=$mysqli->query($sql);
+    if (!$test) 
+    {
+    die("ne pourrait pas se relier : ". $mysqli->error    );
+    }
+   
+
+  
 }
 
 
