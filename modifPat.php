430a0a1be7154b3a6cde6366b73bf464088252b0
Hugo Gineste <hugo.gineste1@gmail.com>
2021-12-14 08:40:44 +0100

    Patient tableau

--- modifPat.php
+++ modifPat.php
@@ -1,54 +1,35 @@
-<?php
-$mysqli = mysqli_connect('localhost', 'root', '','cabinet');
-if (! $mysqli)
-	{
-	die("ne pourrait pas se relier : ". mysqli_error ());
-	}
-?>
-
 <html>
-	<head>
-		<title>Test</title>
-</head>
-<body>
 <?php
-$all="SELECT * FROM patient";
-
-$req = mysqli_query($mysqli,$all);
-
-while ($data = mysqli_fetch_array($req)) {
-	$array= array(
-	 'Nom : ' => array_push($data['Nom']),
-	 'Prenom : ' => array_push( $data['Prenom']),
-	 'Numero de secu : ' =>array_push( $data['Num_secu']),
-	 'Civilite : ' =>array_push( $data['Civilite']),
-	 'Adresse : ' => array_push($data['Adresse']),
-	 'Date de naissance	 : ' => array_push($data['Date_naissance']),
-	 'Lieu de naissance : ' => array_push($data['Lieu_naissance']),
-	 'Numero du medecin : ' => array_push($data['Id_medecin'])
-	);
-	
-}
+$con = new PDO("mysql:host=localhost;dbname=cabinet", 'root',);
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
-<table>
-	<tr> 
-		<th> Nom </th>
-		<th> Prenom </th>
-		<th> Numero de secu </th>
-		<th> Civilite </th>
-		<th> Adresse </th>
-		<th> Date de naissanc </th>
-		<th> Lieu de naissance </th>
-		<th> Numero du medecin </th>
+<table border="1">
+<tr><th>Nom</th><th>Prénom</th><th>Civilité</th><th>Adresse</th><th>Ville</th><th>Date de naissance</th><th>Lieu de naissance</th><th>Numéro de sécu</th><th>Modifier</th><th>Supprimer</th></tr>
+<?php
+$res= $con->prepare("SELECT * FROM patient");
+$res->execute();
 
-	</tr>
-	<tr>
+while ($data = $res->fetch()) {
+	?>
+		
+			 	<tr>
+					<td><?php echo $data['Nom']; ?></td>
+					<td><?php echo $data['Prenom']; ?></td>
+                    <td><?php echo $data['Civilite']; ?></td>
+					<td><?php echo $data['Adresse']; ?></td>
+					<td><?php echo $data['Ville']; ?></td>
+					<td><?php echo $data['Date_naissance']; ?></td>
+                    <td><?php echo $data['Lieu_naissance']; ?></td>
+                    <td><?php echo $data['Num_secu']; ?></td>   
+				</tr>
+			  
 		<?php
-		foreach($array as $value){
-			echo "<th> $value </th>";
-		}
-		?>
-	</tr>
+			}
+			 ///Fermeture du curseur d'analyse des résultats
+			 $res->closeCursor();
+		 ?>
+	
 	
 
+</html>
